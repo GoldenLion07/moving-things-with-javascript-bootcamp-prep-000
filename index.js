1c4f988ce8937dac16015ff6e132c5517ae42596
gmat191@gmail.com <gmat191@gmail.com>
2018-06-19 23:07:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ dodger.style.backgroundColor = "#FF69B4"
 
 dodger.style.left
 
-dodger.style.bottom = '100px'
+dodger.style.bottom = '0px'
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
